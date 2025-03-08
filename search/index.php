00d92c0dc922c25b3b0002adc8fc3bd5f3d14536
AzmiHwamed <70154469+AzmiHwamed@users.noreply.github.com>
2025-03-08 13:04:39 +0100

    db config

---
+++ search/index.php
@@ -0,0 +1,12 @@
+<?php  
+include('./dbconfig/index.php');
+$sql = "SELECT * FROM textjuridique ORDER BY id DESC LIMIT 3";
+$result = $conn->query($sql);
+if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        $id = $row['id'];
+        $date = $row['Date'];
+        $contenu = $row['Contenu'];
+        echo "date : $date \n contenu : $contenu \n ";
+    }
+} ?>
